e99a50eb651f729e63d9ba7804564e44f5267f4a
mridul1001m <mridul1000m@gmail.com>
2025-09-05 10:41:30 +0530

    initial commit

---
+++ payment/payment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace payment
+{
+    
+    interface Ipayment
+    {
+        void Makepayment(double amount);
+    }
+
+    class Creditcard : Ipayment
+    {
+        public void Makepayment(double amount)
+        {
+            Console.WriteLine($"INR {amount} done through credit card");
+        }
+    }
+
+    class Paypal : Ipayment
+    {
+        public void Makepayment(double amount)
+        {
+            Console.WriteLine($"INR {amount} done through paypal");
+        }
+    }
+
+    class UPI : Ipayment
+    {
+        public void Makepayment(double amount)
+        {
+            Console.WriteLine($"INR {amount} done through upi");
+        }
+    }
+
+    class mainprogram
+    {
+        static void Main(string[] args)
+        {
+            Ipayment payment;
+
+            payment = new Creditcard();
+            payment.Makepayment(2000.00);
+
+            payment = new Paypal();
+            payment.Makepayment(1500.50);
+
+            payment = new UPI();
+            payment.Makepayment(1000.75);
+        }
+    }
+}
