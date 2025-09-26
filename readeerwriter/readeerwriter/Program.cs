906b7736b333b9f105debd12e4b8cff4b0a6e469
mridul1001m <mridul1000m@gmail.com>
2025-09-26 09:51:50 +0530

    initial commit

---
+++ readeerwriter/readeerwriter/Program.cs
@@ -0,0 +1,10 @@
+﻿using(StreamWriter writer=new StreamWriter("example.txt"))
+{
+    writer.WriteLine("it is a bmw");
+}
+
+string content=File.ReadAllText("example.txt");
+Console.WriteLine(content);
+File.Copy("example.txt", "data.txt");
+string read = File.ReadAllText("data.txt");
+Console.WriteLine(read);
