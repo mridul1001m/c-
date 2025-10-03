894a7bc7531dfc1d2331c76b278b0cafe1469a39
mridul1001m <mridul1000m@gmail.com>
2025-10-03 10:37:51 +0530

    initial commit

---
+++ list of substring/list of substring/Program.cs
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> findSubstring(const string &s)
+{
+    vector<string> res;
+    for (int i = 0; i < s.length(); i++)
+    {
+        for (int j = i; j < s.length(); j++)
+        {
+            res.push_back(s.substr(i, j - i + 1));
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    string s = "abc";
+    vector<string> res = findSubstring(s);
+    for (const string &i : res) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
