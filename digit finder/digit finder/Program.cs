fdbc5c7a18f45f66358c126ac3d96684d02badaf
mridul1001m <mridul1000m@gmail.com>
2025-10-03 10:35:58 +0530

    initial commit

---
+++ digit finder/digit finder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class DigitFinder
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 0)
+        {
+            number = Math.Abs(number);
+        }
+
+        int lastDigit = number % 10;
+        int firstDigit = number;
+
+        while (firstDigit >= 10)
+        {
+            firstDigit /= 10;
+        }
+
+        Console.WriteLine($"First: {firstDigit}");
+        Console.WriteLine($"Last: {lastDigit}");
+    }
+}
