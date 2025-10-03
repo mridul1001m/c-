816ab5660e82ccf2f5caccd42c20b2e1636a841d
mridul1001m <mridul1000m@gmail.com>
2025-10-03 10:27:10 +0530

    initial commit

---
+++ direct/direct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class FileManager
+{
+    static void Main()
+    {
+        Console.Write("Enter the directory path: ");
+        string path = Console.ReadLine();
+
+        if (Directory.Exists(path))
+        {
+            string[] files = Directory.GetFiles(path);
+
+            Console.WriteLine($"\nFiles in directory '{path}':");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+        else
+        {
+            Console.WriteLine("The specified directory does not exist.");
+        }
+    }
+}
