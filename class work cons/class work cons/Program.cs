61f4b5eee5e56045a29000f2c23ebfc61d5d3fc3
mridul1001m <mridul1000m@gmail.com>
2025-10-03 10:30:48 +0530

    initial commit

---
+++ class work cons/class work cons/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            foreach (char digitchar in input)
+            {
+                if (char.IsDigit(digitchar))
+                {
+                    sum += int.Parse(digitchar.ToString());
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}"); 
+            Console.ReadLine();
+        }
+    }
+}
