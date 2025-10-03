7290877b704cbd3646c31bcc4f5037b860e60f6f
mridul1001m <mridul1000m@gmail.com>
2025-10-03 10:31:37 +0530

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public class point
+        {
+            internal void Display()
+            {
+                Console.WriteLine("ghgf");
+            }
+        }
+    }
+}
