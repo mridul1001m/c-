5409ef68069cbbb4bee5784cd48711cf1edd2357
mridul1001m <mridul1000m@gmail.com>
2025-10-10 10:19:10 +0530

    initial commit

---
+++ stacksdelegate/stacksdelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Stack<string> stacks = new Stack<string>();
+        stacks.Push("alice");
+        stacks.Push("is");
+        stacks.Push("dumb");
+        stacks.Push("why");
+        stacks.Pop();
+
+        Console.WriteLine("Stack contents after popping:");
+        foreach (string item in stacks)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
