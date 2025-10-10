3d0744658836a20db25cd71a9ebbee856547d1df
mridul1001m <mridul1000m@gmail.com>
2025-10-10 10:19:44 +0530

    initial commit

---
+++ queuedelegate/queuedelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("the");
+        queue.Enqueue("book");
+        queue.Enqueue("is");
+        queue.Enqueue("fanta");
+
+        queue.Dequeue();
+
+        Console.WriteLine("Queue contents after dequeuing:");
+        foreach (string item in queue)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
