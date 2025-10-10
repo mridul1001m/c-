f59bbafee8959ef8a663eb260fe8a96eb96427b4
mridul1001m <mridul1000m@gmail.com>
2025-10-10 10:31:07 +0530

    initial commit

---
+++ multicastingdelegate/multicastingdelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+   
+  delegate void Phonebook();
+
+    
+    public static void page1()
+    {
+        Console.WriteLine("translate");
+    }
+
+    public static void page2()
+    {
+        Console.WriteLine("cant say");
+    }
+
+    public static void Main()
+    {
+       
+       Phonebook notify = page1;
+        notify += page2;
+        notify(); 
+    }
+}
